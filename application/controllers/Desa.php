92248ab8c83e81f1e300bce5f19187221a7fe930
syarifonta <49420084+syarifonta@users.noreply.github.com>
2019-05-13 13:43:27 +0700

    Add files via upload
    update data desa

---
+++ application/controllers/Desa.php
@@ -0,0 +1,12 @@
+<?php 
+
+class Desa extends CI_Controller{
+
+	public function index(){
+
+		$data['data_desa']=$this->desa_model->getDesa();
+		$data['main_admin']="content/Desa";
+		$this->load->view('main/dashboard',$data);
+	}
+}
+?>
