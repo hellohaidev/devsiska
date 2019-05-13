606c8d6c85007004e70f52b3f8339dc6e49aa567
hellohaidev <hello.devapps@gmail.com>
2019-05-13 14:21:40 +0700

    menampilkan data total desa di dashboard

--- application/views/content/main.php
+++ application/views/content/main.php
@@ -15,5 +15,20 @@
             <a href="<?php echo base_url() ?>total_kegiatan" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
+
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-green">
+            <div class="inner">
+              <h3><?php echo $total_desa ?></h3>
+
+              <p>Total Desa</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
     </div>
 </section>
