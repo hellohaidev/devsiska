07b176de99ba8c4b959002cf8387245b44f3726d
hellohaidev <hello.devapps@gmail.com>
2019-05-13 11:45:39 +0700

    update dashboard view

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -4,7 +4,7 @@ class Dashboard extends CI_Controller {
 
     public function index(){
         
-        $data['data_kegiatan'] = $this->kegiatan_model->getKegiatan();
+        // $data['data_kegiatan'] = $this->kegiatan_model->getKegiatan();
         $data['main_admin'] = "content/main"; 
         $this->load->view('main/dashboard',$data);
     }
