3e212ff6c6cc4ad8917e1f0905153e2768463378
hellohaidev <hello.devapps@gmail.com>
2019-05-13 14:19:32 +0700

    menampilkan total desa di dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,6 +5,7 @@ class Dashboard extends CI_Controller {
     public function index(){
         
     	$data['total_kegiatan'] = $this->dashboard_model->get_count_kegiatan();
+    	$data['total_desa'] = $this->dashboard_model->get_count_desa();
         $data['main_admin'] = "content/main"; 
         $this->load->view('main/dashboard',$data);
     }
