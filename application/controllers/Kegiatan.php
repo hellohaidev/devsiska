dfef27c9b0b6e661dd43b60ae9681d503820ea1d
hellohaidev <hello.devapps@gmail.com>
2019-05-13 09:59:04 +0700

    upload kegiatan controller

---
+++ application/controllers/Kegiatan.php
@@ -0,0 +1,13 @@
+<?php
+
+class Kegiatan extends CI_Controller {
+
+    public function index(){
+        
+        $data['data_kegiatan'] = $this->kegiatan_model->getKegiatan();
+        $data['main_admin'] = "content/main"; 
+        $this->load->view('main/dashboard',$data);
+    }
+
+   
+}
