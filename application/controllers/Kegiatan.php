4a0687680cd63ccd995e1d496926a90396f989ab
hellohaidev <hello.devapps@gmail.com>
2019-05-13 11:46:13 +0700

    update view controller kegiatan

--- application/controllers/Kegiatan.php
+++ application/controllers/Kegiatan.php
@@ -5,7 +5,7 @@ class Kegiatan extends CI_Controller {
     public function index(){
         
         $data['data_kegiatan'] = $this->kegiatan_model->getKegiatan();
-        $data['main_admin'] = "content/main"; 
+        $data['main_admin'] = "content/kegiatan"; 
         $this->load->view('main/dashboard',$data);
     }
 
