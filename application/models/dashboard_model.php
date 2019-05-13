6da694b6495a011672f550d1a6a0834fa0f5f2f1
hellohaidev <hello.devapps@gmail.com>
2019-05-13 14:20:29 +0700

    update dashboard_model query count_desa

--- application/models/dashboard_model.php
+++ application/models/dashboard_model.php
@@ -6,8 +6,22 @@
 class Dashboard_model extends CI_Model
 {
 	
+	/*
+		Total Kecamatan
+	*/
 	public function get_count_kegiatan(){
 		$query_count = $this->db->count_all_results('Ref_Kegiatan');
 		return $query_count;
 	}
+
+	/*
+		Total Desa
+	*/
+
+	public function get_count_desa(){
+		$query_count = $this->db->count_all_results('Ref_Desa');
+		return $query_count;
+	}
+
+
 }
