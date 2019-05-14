6df9049361b4fd2888de3674fdf11a8fe2643c0a
syarifonta <syarifonta@gmail.com>
2019-05-14 13:55:58 +0700

    update main

--- application/views/content/main.php
+++ application/views/content/main.php
@@ -30,5 +30,21 @@
             <a href="<?php echo base_url() ?>Kecamatan" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
+
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-red">
+            <div class="inner">
+              <h3><?php echo $total_desa ?></h3>
+
+              <p>Total Desa</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="<?php echo base_url(); ?>Desa" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+
     </div>
 </section>
