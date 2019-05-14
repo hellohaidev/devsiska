cf27867362dc9bccde7127f4ec80aeeb8ae80eab
syarifonta <syarifonta@gmail.com>
2019-05-14 13:46:27 +0700

    update Dashboard

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -6,6 +6,7 @@ class Dashboard extends CI_Controller {
         
     	$data['total_kegiatan'] = $this->dashboard_model->get_count_kegiatan();
     	$data['total_kecamatan'] = $this->dashboard_model->get_count_kecamatan();
+    	$data['total_desa'] = $this->dashboard_model->get_count_desa();
         $data['main_admin'] = "content/main"; 
         $this->load->view('main/dashboard',$data);
     }
