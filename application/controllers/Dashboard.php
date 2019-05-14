ad42eb228bb2b18fe0f4dc04f839e0fdfbf13b51
hellohaidev <hello.devapps@gmail.com>
2019-05-14 08:56:05 +0700

    rename method total_desa menjadi total_kecamatan

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -5,7 +5,7 @@ class Dashboard extends CI_Controller {
     public function index(){
         
     	$data['total_kegiatan'] = $this->dashboard_model->get_count_kegiatan();
-    	$data['total_desa'] = $this->dashboard_model->get_count_desa();
+    	$data['total_kecamatan'] = $this->dashboard_model->get_count_kecamatan();
         $data['main_admin'] = "content/main"; 
         $this->load->view('main/dashboard',$data);
     }
