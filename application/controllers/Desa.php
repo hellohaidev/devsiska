0b0679213d7da69c80dc046ba1a83682bc7b8dbd
syarifonta <syarifonta@gmail.com>
2019-05-14 13:47:23 +0700

    update desa

--- application/controllers/Desa.php
+++ application/controllers/Desa.php
@@ -5,7 +5,7 @@ class Desa extends CI_Controller{
 	public function index(){
 
 		$data['data_desa']=$this->desa_model->getDesa();
-		$data['main_admin']="content/Desa";
+		$data['main_admin']="content/desa";
 		$this->load->view('main/dashboard',$data);
 	}
 }
