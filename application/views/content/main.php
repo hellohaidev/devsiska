d91bc7885edea53ef6aba1ee9828b49a8e7e7034
hellohaidev <hello.devapps@gmail.com>
2019-05-14 08:59:04 +0700

    menampilkan total kecamatan

--- application/views/content/main.php
+++ application/views/content/main.php
@@ -12,7 +12,7 @@
             <div class="icon">
               <i class="ion ion-bag"></i>
             </div>
-            <a href="<?php echo base_url() ?>total_kegiatan" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="<?php echo base_url() ?>Kegiatan" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
 
@@ -20,14 +20,14 @@
           <!-- small box -->
           <div class="small-box bg-green">
             <div class="inner">
-              <h3><?php echo $total_desa ?></h3>
+              <h3><?php echo $total_kecamatan ?></h3>
 
-              <p>Total Desa</p>
+              <p>Total Kecamatan</p>
             </div>
             <div class="icon">
               <i class="ion ion-bag"></i>
             </div>
-            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            <a href="<?php echo base_url() ?>Kecamatan" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
           </div>
         </div>
     </div>
