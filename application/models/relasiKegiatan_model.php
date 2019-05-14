929beb334cb72e023d7cbf37c3bc39d9404fddb8
hellohaidev <hello.devapps@gmail.com>
2019-05-14 10:44:11 +0700

    query detail kegiatan

--- application/models/relasiKegiatan_model.php
+++ application/models/relasiKegiatan_model.php
@@ -5,14 +5,17 @@
  */
 class relasiKegiatan_model extends CI_Model
 {
-	public function get_relasi_kegiatan(){
 
-		$query = $this->db->query('
-				SELECT  *  FROM dbo.Ref_Kegiatan 
-				INNER JOIN dbo.Ta_Kegiatan 
-				ON dbo.Ref_Kegiatan.ID_Keg = dbo.Ta_Kegiatan.ID_Keg
-			');
-		return $query->result();
+
+	public function get_relasi_kegiatan_by_id($id){
+
+		$this->db->select('*');
+        $this->db->from('Ref_Kegiatan');
+        $this->db->join('Ta_Kegiatan', 'Ta_Kegiatan.ID_Keg = Ref_Kegiatan.ID_Keg','inner');
+        $this->db->where('Ref_Kegiatan.ID_Keg',$id);
+        $dataInfo = $this->db->get();
+       
+		return $dataInfo->result();
 
 
 	}
