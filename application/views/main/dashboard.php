f3cde4e31a49d73d3dcfa27c49a9ddae651e2512
hellohaidev <hello.devapps@gmail.com>
2019-05-14 09:20:24 +0700

    menambahkan menu data relasi table kegiatan

--- application/views/main/dashboard.php
+++ application/views/main/dashboard.php
@@ -84,6 +84,19 @@
             
           </ul>
         </li>
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa-dashboard"></i> <span>Data Relasi</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+            </a>
+          <ul class="treeview-menu">
+            <li class="active"><a href="<?php echo base_url() ?>RelasiKegiatan"><i class="fa fa-circle-o"></i> Kegiatan</a></li>
+           
+            
+          </ul>
+        </li>
         <li><a href="https://adminlte.io/docs"><i class="fa fa-book"></i> <span>Documentation</span></a></li>
         <li class="header">Configuration</li>
         <li><a href="#"><i class="fa fa-circle-o text-red"></i> <span>Settings</span></a></li>
