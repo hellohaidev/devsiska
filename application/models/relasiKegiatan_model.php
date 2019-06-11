c46e36b22c8cdf2172c62a9eafb1c29d5eae883f
hellohaidev <hello.devapps@gmail.com>
2019-06-11 09:56:25 +0700

    menambahkan method model total anggaran,total volume,info detail

--- application/models/relasiKegiatan_model.php
+++ application/models/relasiKegiatan_model.php
@@ -17,6 +17,68 @@ class relasiKegiatan_model extends CI_Model
        
 		return $dataInfo->result();
 
-
 	}
+
+	public function get_realisasi_detail_by_kode_desa_info($idKeg,$idDesa){
+		$this->db->select('*');
+        $this->db->from('Ta_KegiatanOutput');
+        $this->db->join('Ta_Kegiatan', 'Ta_Kegiatan.Kd_Desa = Ta_KegiatanOutput.Kd_Desa','inner');
+        $this->db->where('Ta_Kegiatan.ID_Keg',$idKeg);
+        $this->db->where('Ta_KegiatanOutput.Kd_Desa',$idDesa);
+      	
+        $dataInfoRealisasi = $this->db->get();
+       
+		return $dataInfoRealisasi->row();
+	}
+
+	public function get_realisasi_detail_by_kode_desa($idKeg,$idDesa){
+		$this->db->select('*');
+        $this->db->from('Ta_KegiatanOutput');
+        $this->db->join('Ta_Kegiatan', 'Ta_Kegiatan.Kd_Desa = Ta_KegiatanOutput.Kd_Desa','inner');
+        $this->db->where('Ta_Kegiatan.ID_Keg',$idKeg);
+        $this->db->where('Ta_KegiatanOutput.Kd_Desa',$idDesa);
+      	
+        $dataInfoRealisasi = $this->db->get();
+       
+		return $dataInfoRealisasi->result();
+	}
+
+	public function get_sum_anggaran_realisasi_kegiatan($idKeg,$idDesa){
+		$this->db->select_sum('Anggaran');
+        $this->db->from('Ta_KegiatanOutput');
+        $this->db->join('Ta_Kegiatan', 'Ta_Kegiatan.Kd_Desa = Ta_KegiatanOutput.Kd_Desa','inner');
+        $this->db->where('Ta_Kegiatan.ID_Keg',$idKeg);
+        $this->db->where('Ta_KegiatanOutput.Kd_Desa',$idDesa);
+		$query = $this->db->get();
+		
+      	
+		if ($query->num_rows()>0)
+		{
+			return $query->row()->Anggaran;
+		}
+		else
+		{
+			return 0;
+		}			
+	}
+	public function get_sum_volume_realisasi_kegiatan($idKeg,$idDesa){
+		$this->db->select_sum('Volume');
+        $this->db->from('Ta_KegiatanOutput');
+        $this->db->join('Ta_Kegiatan', 'Ta_Kegiatan.Kd_Desa = Ta_KegiatanOutput.Kd_Desa','inner');
+        $this->db->where('Ta_Kegiatan.ID_Keg',$idKeg);
+        $this->db->where('Ta_KegiatanOutput.Kd_Desa',$idDesa);
+		$query = $this->db->get();
+		
+      	
+		if ($query->num_rows()>0)
+		{
+			return $query->row()->Volume;
+		}
+		else
+		{
+			return 0;
+		}			
+	}
+
+	
 }
